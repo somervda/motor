d267769c25f6a897654548a82059d32d139fa6b9
David <39265971+somervda@users.noreply.github.com>
2021-02-24 16:32:10 -0500

    Clean up L298_tester, changed back to using a sleep in the main program flow

--- uPython/L298_tester.py
+++ uPython/L298_tester.py
@@ -1,12 +1,9 @@
 from L298 import L298
-import time
-
-from machine import Pin, I2C, Timer
 from ssd1306 import SSD1306_I2C
-import framebuf
+from machine import Pin, I2C
+import time
 import gc
 from dataLogger import DataLogger
-import _thread
 import umemory
 
 # Set up OLED display interface
@@ -16,16 +13,12 @@ i2c = I2C(0)
 # Uses I2C defaults for I2C0 SCL=Pin(GP9), SDA=Pin(GP8)
 oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)
 
-
 # Set up motor encoder interface
 gMotorEncoderCnt = 0
 motorEncoder = Pin(10, Pin.IN)
 
-statusTimer = Timer()
-
-gSpeed = 0
-gIsForward = True
-gDuration = 0
+#  Set globals
+gDuration = 3
 gFreq = 300
 gCycle = 0
 
@@ -39,32 +32,12 @@ def motorEncoderCallback(pin):
     gMotorEncoderCnt += 1
 
 
-def stopMoving(timer):
-    # Reset motorEncoder and handler
-    motorEncoder.irq(handler=None)
-    my298.stop()
-    motorEncoder.init()
-    print("stopMoving")
-    global gMotorEncoderCnt
-    global gDuration
-    global gSpeed
-    global gIsForward
-    global gFreq
-    #motorEncoder.irq(trigger=None, handler=None)
-    dl.write(str(gCycle) + "\t" + str(gSpeed) + "\t" + str(gIsForward) + "\t" +
-             str(gMotorEncoderCnt) + "\t" + str(gDuration) + "\t" + str(gFreq) + "\tL298")
-    gc.collect()
-    gMotorEncoderCnt = 0
-
-
 def setSpeed(speed, isForward=True):
     global gDuration
-    global gSpeed
-    global gIsForward
     global gCycle
-    gDuration = 3
-    gSpeed = speed
-    gIsForward = isForward
+    global gFreq
+    global gMotorEncoderCnt
+
     oled.fill(0)
     oled.text("speed: {} ".format(speed), 5, 5)
     oled.text("forward: {}".format(isForward), 5, 15)
@@ -72,8 +45,6 @@ def setSpeed(speed, isForward=True):
     print("speed: {} isForward?: {} free:{} cycle:{}".format(
         speed, isForward, umemory.free(), gCycle))
 
-    statusTimer.init(freq=1/gDuration, mode=Timer.ONE_SHOT,
-                     callback=stopMoving)
     # Run for the sleepFor seconds
     motorEncoder.irq(trigger=machine.Pin.IRQ_FALLING,
                      handler=motorEncoderCallback)
@@ -81,10 +52,20 @@ def setSpeed(speed, isForward=True):
         my298.forward(speed)
     else:
         my298.reverse(speed)
-    # time.sleep(sleepFor)
+    time.sleep(gDuration)
+    # Reset motorEncoder and handler
+    # Stop the irq handeler before doing anything else!
+    motorEncoder.irq(handler=None)
+    my298.stop()
+    motorEncoder.init()
+    #  Write out log and cleanup gc, and encoder counter
+    dl.write(str(gCycle) + "\t" + str(speed) + "\t" + str(isForward) + "\t" +
+             str(gMotorEncoderCnt) + "\t" + str(gDuration) + "\t" + str(gFreq) + "\tL298")
+    gc.collect()
+    gMotorEncoderCnt = 0
+    # time.sleep(1)
 
-    #  Reset the motor encoder cnt
-#     motorEncoderCnt = 0
+# *********** Main code **************
 
 
 my298 = L298(11, 12, 13, gFreq)
@@ -92,23 +73,11 @@ dl = DataLogger("cycle\tspeed\tisForward\tmotorEncoderCnt\tduration\tfreq\tmc")
 
 while True:
     gCycle += 1
-    my298.stop()
-    oled.fill(0)
-    oled.text("Stopped!", 5, 5)
-    oled.show()
-    time.sleep(3)
-    # setSpeed(20)
+    setSpeed(0)
     setSpeed(100)
-    time.sleep(5)
     setSpeed(80)
-    time.sleep(5)
     setSpeed(60)
-    time.sleep(5)
     setSpeed(40)
-    time.sleep(5)
     setSpeed(20)
-    time.sleep(5)
     setSpeed(10)
-    time.sleep(5)
     setSpeed(5)
-    time.sleep(5)
